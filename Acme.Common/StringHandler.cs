50962aa643f60f74c945ea65440e839a208dcd83
luiz@sistemasbr.net <luiz@sistemasbr.net>
2019-09-14 11:28:57 -0300

    Criando projeto em comum

---
+++ Acme.Common/StringHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Acme.Common
+{
+    public class StringHandler
+    {
+        public string InsertSpaces(string source) => 
+            string.IsNullOrWhiteSpace(source)
+            ? string.Empty
+            : source.Aggregate(string.Empty,
+                (current, letter) => current + (char.IsUpper(letter) ? $" {letter}" : letter.ToString()));
+    }
+}
